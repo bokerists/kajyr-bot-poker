ba8af984800e01236860f9964f21a6315be3d676
kajyr <carlo.panzi@gmail.com>
2016-03-22 22:00:33 +0100

    arcangelo pensaci tu

--- player.js
+++ player.js
@@ -1,94 +1,93 @@
-'use strict';
-
-var _ = require("lodash");
-var c = require("./cards.js");
-var highCards = ['K', 'J', 'Q', 'A'];
-
-var mapVal = {
-  '2': 1,
-  '3': 1,
-  '4': 1,
-  '5': 1,
-  '6': 1,
-  '7': 1,
-  '8': 1,
-  '9': 1,
-  '10': 3,
-  'J': 4,
-  'Q': 6,
-  'K': 8,
-  'A': 10,
-}
-
-var turniInDue = 0;
 
 exports = module.exports = {
 
   VERSION: 'Superstar poker player',
 
-  bet: function (gamestate, bet) {
+  myCard: null,
+  commonCards: null,
+  myId: null,
+  player: null,
+  betValue:Infinity,
 
-    var me = gamestate.players[gamestate.me];
-    var hand = gamestate.commonCards.concat(me.cards);
-    var numPlayers = _.filter(gamestate.players, function(g) { return g.status == 'active'} ).length;
-    
-    var river = gamestate.commonCards.length === 5; 
-    var turn = gamestate.commonCards.length === 4;
-    var preRiver = gamestate.commonCards.length < 5;
-    var preTurn = gamestate.commonCards.length < 4;
-    var preFlop = gamestate.commonCards.length < 3;
-
-    var allInPlayers = _.filter(gamestate.players, function(g) { return g.chips == 0 } ).length;
-    var somebodyAllInPreTurn = preTurn && allInPlayers > 0;
-    
-
-    var call = _.partial(bet, gamestate.callAmount);
-    var fold = _.partial(bet, 0);
-    var allIn = _.partial(bet, Infinity);
-
-    // What we have
-    var pair = c.hasPair(hand);
-    var pairInHand = c.hasPair(me.cards);
-    var tris = c.hasTris(hand);
-    var poker = c.hasPoker(hand);
-    var color = c.hasColor(hand);
-    var highPair = highCards.indexOf(pair) >= 0;
-    var highTris = highCards.indexOf(tris) >= 0;
-    var nonHoUnCazzo = !pair && !color;
-
-    if (gamestate.spinCount === 0) {
-      var puntate = [];
-    }
-
-    if (nonHoUnCazzo && gamestate.spinCount >= 5) {
-      if (somebodyAllInPreTurn) { return fold(); }
-      if (numPlayers === 2) { return fold(); }
-      if (!preFlop) { return fold(); }
-    }
-    var ourBet = 1;
-
-    if (highTris || highPair || color) {
-      ourBet = 2;
-    }
-
-    if (numPlayers === 2) {
-      ourBet *= 2;
-    }
-
-    var b = Math.max(gamestate.callAmount, gamestate.sb * ourBet);
-    
-    if (puntate.length > 3) {
-      var last = puntate[puntate.length - 1];
-      var prev = puntate[puntate.length - 2];
-
-      if (b == last && last == prev) {
-        b = Infinity;
+  getMyCard: function (gamestate) {
+    gamestate.players.forEach( function(v){
+      if(v.id == gamestate.me) {
+        this.myCard = v.cards;
+        this.player = v;
       }
-    }
-   
+    }.bind(this) );
+  },
 
-    puntate.push(b);
-    return bet(b);
+  getBet: function (gamestate) {
+    if(this.isPair() && this.isPreFlop())
+      return gamestate.pot*1.2;
+
+    if(this.isPair() )
+      return gamestate.pot*1.2;
+
+    if(this.areFigures())
+      return gamestate.callAmount;
+
+    if(this.isThereAnyFigure())
+      return gamestate.pot;
+
+    return gamestate.sb*3;
+  },
+  
+  isPair: function(){
+    return (this.myCard[0].rank == this.myCard[1].rank)
+  },
+
+  isPreFlop: function(){
+    return (this.commonCards.length==0);
+  },
+
+  isThereAnyFigure: function(){
+    if(isNaN(this.myCard[0].rank*1) || isNaN(this.myCard[1].rank*1))
+      return true;
+  },
+  
+  areFigures: function(){
+    if(isNaN(this.myCard[0].rank*1) && isNaN(this.myCard[1].rank*1))
+      return true;
+  },
+
+  cardToNumber: function(cardRank) {
+  if(parseInt(cardRank).toString() != "NaN"){
+    return cardRank;
+    }
+  if(cardRank==='J')return '11';
+    if(cardRank==='Q')return '12';
+    if(cardRank==='K')return '13';
+    if(cardRank==='A')return '14';
+  },
+
+  bet: function (gamestate, bet) {
+    'use strict';
+debugger;
+    // gamestate contains info about the state of the game.
+    // check the documentation at https://bot-poker.herokuapp.com/wiki for further info about the data structure.
+
+    //
+    // bet is the function you should use to send your bet.
+
+    this.myId = gamestate.me;
+
+    this.getMyCard(gamestate);
+    // enjoy the game!
+
+    this.commonCards = gamestate.commonCards;
+
+
+    this.betValue = this.getBet(gamestate);
+
+
+    //
+    // currently we just go all-in every single hand!
+
+
+
+    return bet(this.betValue);
 
   }
 
